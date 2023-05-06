846f3137b87ff2f7195cf668a33d2c642c6580f5
Samuel Gonzalez Ruiz <sgruiz462@gmail.com>
2023-05-07 00:08:48 +0200

    Added toastify notifications

--- src/services/httpService.js
+++ src/services/httpService.js
@@ -1,22 +1,22 @@
-import axios from "axios";
+import axios from 'axios'
+import {toast} from 'react-toastify'
 
-axios.interceptors.response.use(null, (error) => {
-  const expectedError =
-    error.response &&
-    error.response.status >= 400 &&
-    error.response.status < 500;
-
-  if (!expectedError) {
-    console.log("Logging the error", error);
-    alert("An unexpected error has occurred.");
-  }
-
-  return Promise.reject(error);
-});
-
-export default {
-  get: axios.get,
-  put: axios.put,
-  post: axios.post,
-  delete: axios.delete,
-};
+axios.interceptors.response.use(null, error => {
+    const expectedError =
+      error.response &&
+      error.response.status >= 400 &&
+      error.response.status < 500;
+  
+    if (!expectedError) {
+      toast("An unexpected error occurrred.");
+    }
+  
+    return Promise.reject(error);
+  });
+  
+  export default {
+    get: axios.get,
+    post: axios.post,
+    put: axios.put,
+    delete: axios.delete
+  };
